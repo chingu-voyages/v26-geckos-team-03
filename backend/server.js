b93428aaa3b620e4efb1ac5ed9467b59886dcddd
upluke <lukeforadream@gmail.com>
2021-01-18 17:52:53 -0800

    clearCache

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,6 @@ import userRoutes from './routes/userRoutes.js'
 
 const app =express()
 dotenv.config()
-
-// app.use(bodyParser.json({ limit: "30mb", extended: true }));  
-// app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors())
 
 app.use('/users',userRoutes)
