6c447c6ad1da70e383ccaa258a08d4f80d3895c6
upluke <lukeforadream@gmail.com>
2021-02-02 23:45:51 -0800

    cardSchemaUpdate

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,22 +1,20 @@
 import mongoose from "mongoose"
 
 const userSchema = mongoose.Schema({
-    firstName:{
+    userName:{
         type:String,
-        require:true,
+        require:[true,"can't be blank"],
         trim:true,
-        minlength:2
+        minlength:2,
+        maxlength:125,
+        index:true,
     },
-    lastName:{
-        type:String,
-        require:true,
-        trim:true,
-        minlength:2
-    },
+    slug:{type: String, slug:"userName"},
     email:{
         type:String,
         require:true,
-        unique:true,
+        unique:[true,"can't be blank"],
+        match:[/\S+@\S+\.\S+/, 'is invalid'],
         lowercase: true
     },
     password:{
